43caf0f0dd2bd2562d0709dccd00d9edc60e85e0
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-09-08 09:41:08 -0700

    update2

--- main.js
+++ main.js
@@ -1,35 +1,3 @@
-// function showTextOne() {
-//   document.querySelector("#text-one").style.display = "block";
-//   document.querySelector("#text-two").style.display = "none";
-//   document.querySelector("#text-three").style.display = "none";
-// }
-// function showTextTwo() {
-//   document.querySelector("#text-two").style.display = "block";
-//   document.querySelector("#text-one").style.display = "none";
-//   document.querySelector("#text-three").style.display = "none";
-// }
-// function showTextThree() {
-//   document.querySelector("#text-three").style.display = "block";
-//   document.querySelector("#text-two").style.display = "none";
-//   document.querySelector("#text-one").style.display = "none";
-// }
-
-// function showTextOne() {
-//   document.querySelector("#text-one").classList.toggle("textExpand");
-//   document.querySelector("#text-two").style.display = "none";
-//   document.querySelector("#text-three").style.display = "none";
-// }
-// function showTextTwo() {
-//   document.querySelector("#text-two").classList.toggle("textExpand");
-//   document.querySelector("#text-one").style.display = "none";
-//   document.querySelector("#text-three").style.display = "none";
-// }
-// function showTextThree() {
-//   document.querySelector("#text-three").classList.toggle("textExpand");
-//   document.querySelector("#text-two").style.display = "none";
-//   document.querySelector("#text-one").style.display = "none";
-// }
-
 let acc = document.getElementsByClassName("accordion");
 let i;
 for (i = 0; i < acc.length; i++) {
@@ -50,3 +18,19 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+// function showTextOne() {
+//   document.querySelector("#text-one").style.display = "block";
+//   document.querySelector("#text-two").style.display = "none";
+//   document.querySelector("#text-three").style.display = "none";
+// }
+// function showTextTwo() {
+//   document.querySelector("#text-two").style.display = "block";
+//   document.querySelector("#text-one").style.display = "none";
+//   document.querySelector("#text-three").style.display = "none";
+// }
+// function showTextThree() {
+//   document.querySelector("#text-three").style.display = "block";
+//   document.querySelector("#text-two").style.display = "none";
+//   document.querySelector("#text-one").style.display = "none";
+// }
