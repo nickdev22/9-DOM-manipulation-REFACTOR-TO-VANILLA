6eea630477a6e9edd2d7d06e2241766b5cdbbe64
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-09-08 09:38:41 -0700

    update

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-function showTextOne() {
-  document.querySelector("#text-one").style.display = "block";
-  document.querySelector("#text-two").style.display = "none";
-  document.querySelector("#text-three").style.display = "none";
-}
-function showTextTwo() {
-  document.querySelector("#text-two").style.display = "block";
-  document.querySelector("#text-one").style.display = "none";
-  document.querySelector("#text-three").style.display = "none";
-}
-function showTextThree() {
-  document.querySelector("#text-three").style.display = "block";
-  document.querySelector("#text-two").style.display = "none";
-  document.querySelector("#text-one").style.display = "none";
-}
+// function showTextOne() {
+//   document.querySelector("#text-one").style.display = "block";
+//   document.querySelector("#text-two").style.display = "none";
+//   document.querySelector("#text-three").style.display = "none";
+// }
+// function showTextTwo() {
+//   document.querySelector("#text-two").style.display = "block";
+//   document.querySelector("#text-one").style.display = "none";
+//   document.querySelector("#text-three").style.display = "none";
+// }
+// function showTextThree() {
+//   document.querySelector("#text-three").style.display = "block";
+//   document.querySelector("#text-two").style.display = "none";
+//   document.querySelector("#text-one").style.display = "none";
+// }
 
 // function showTextOne() {
 //   document.querySelector("#text-one").classList.toggle("textExpand");
@@ -29,3 +29,24 @@ function showTextThree() {
 //   document.querySelector("#text-two").style.display = "none";
 //   document.querySelector("#text-one").style.display = "none";
 // }
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    let panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      let active = document.querySelectorAll(
+        ".accordion-div .accordion.active"
+      );
+      for (let j = 0; j < active.length; j++) {
+        active[j].classList.remove("active");
+        active[j].nextElementSibling.style.maxHeight = null;
+      }
+      this.classList.toggle("active");
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
